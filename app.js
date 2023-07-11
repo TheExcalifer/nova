844a70c3205ffa5bb799539264102cf5dcdc2824
TheVelius <TheAellius@gmail.com>
2023-07-11 19:16:19 +0330

    Allow Origin, Methods, Headers

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(bodyParser.json());
 
 // routes
